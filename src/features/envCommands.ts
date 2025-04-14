e82d4b5274a39915606675e745c8b6236c962958
Eleanor Boyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-04-14 10:50:26 -0700

    Add fix to support project root item in project getter (#298)

--- src/features/envCommands.ts
+++ src/features/envCommands.ts
@@ -348,6 +348,10 @@ export async function addPythonProject(
         return;
     }
 
+    if (resource instanceof ProjectPackageRootTreeItem || resource instanceof ProjectPackage) {
+        await addPythonProject(undefined, wm, em, pc);
+    }
+
     if (resource instanceof Uri) {
         const uri = resource as Uri;
         const envManagerId = getDefaultEnvManagerSetting(wm, uri);
